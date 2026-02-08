8f631f216eb8a4d22040ee812f3070121ea4345c
john <jonathan.fernandez.f@usach.cl>
2026-02-08 13:43:25 -0300

    anadiendo vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { Analytics } from '@vercel/analytics/next';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Projects />
         <Experience />
         <Contact />
+        <Analytics />
       </main>
       <Footer />
     </div>
